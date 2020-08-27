5b1055f2f385775f7efa910394851a6cf65001bd
Junior Fracassi <ricardo.jr1999@hotmail.com>
2020-08-27 17:42:14 -0300

    Colocando swagger

---
+++ ApiRest/src/main/java/com/junior/ApiRest/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.junior.ApiRest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.junior.ApiRest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        var apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos",
+                "1.0",
+                "Termos de serviço",
+                new Contact("Ricardo Fracassi", null, "ricardo.jr1999@hotmail.com"),
+                "Apache Licence Version 2.0",
+                "https://www.apache.org/licensen.html", new ArrayList<VendorExtension>());
+        return apiInfo;
+    }
+
+
+}
--- ApiRest/src/main/java/com/junior/ApiRest/resources/ProdutoResource.java
+++ ApiRest/src/main/java/com/junior/ApiRest/resources/ProdutoResource.java
@@ -2,6 +2,8 @@ package com.junior.ApiRest.resources;
 
 import com.junior.ApiRest.models.Produto;
 import com.junior.ApiRest.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,34 +11,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST Produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoResource {
 
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de produtos do banco de dados")
     public List<Produto> listaProdutos() {
         return produtoRepository.findAll();
 
     }
 
     @GetMapping("/produto/{id}")
+    @ApiOperation(value = "Retorna um unico produto no banco de dados")
     public Produto listaProdutoUnico(@PathVariable(value = "id") long id) {
         return produtoRepository.findById(id);
     }
 
     @PostMapping("/produto")
+    @ApiOperation(value = "Salva um produto no banco de dados")
     public Produto salvaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
 
     }
 
     @DeleteMapping("/produto")
+    @ApiOperation(value = "Deleta um produto no banco de dados")
     public void deletaProduto(@RequestBody Produto produto){
         produtoRepository.delete(produto);
     }
 
     @PutMapping("/produto")
+    @ApiOperation(value = "Edita um produto no banco e dados")
     public Produto atualizaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
 
